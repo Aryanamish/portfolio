2f50f3503d9e56b693204a41fc78035c3acaa0c2
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-02-02 10:42:18 +0530

    initial Commit

---
+++ src/socials/Socials.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Socials() {
+	return (
+		<div>
+			<div className="flex w-full h-56">
+				<div className="w-10 bg-skin-fill-highlight"></div>
+				<div className="flex flex-col">
+					<div className="bg-skin-fill-highlight">hi</div>
+					<div className="bg-skin-fill-highlight">hello</div>
+				</div>
+				<div className="w-10 bg-skin-fill-highlight"></div>
+			</div>
+		</div>
+	);
+}
+
+export default Socials;

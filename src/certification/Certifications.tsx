df3f7793190084509475a75306df1142c8692e71
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2023-05-29 23:59:56 +0530

    ran npm audit

--- src/certification/Certifications.tsx
+++ src/certification/Certifications.tsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
-import Data from '../Data';
-import Cert from './Cert';
+// import React, {useState} from 'react';
+// import Data from '../Data';
+// import Cert from './Cert';
 import Certificate2 from './Certificate2';
 function Certifications() {
-	const [certData, setCertData] = useState(0);
+	// const [certData, setCertData] = useState(0);
 
-	const setNext = () => {
-		setCertData((certData + 1) % Data.certificates.length);
-	};
-	const setPrev = () => {
-		let new_num = (certData - 1) % Data.certificates.length;
-		if (new_num < 0) {
-			new_num += Data.certificates.length;
-		}
-		setCertData(new_num);
-	};
+	// const setNext = () => {
+	// 	setCertData((certData + 1) % Data.certificates.length);
+	// };
+	// const setPrev = () => {
+	// 	let new_num = (certData - 1) % Data.certificates.length;
+	// 	if (new_num < 0) {
+	// 		new_num += Data.certificates.length;
+	// 	}
+	// 	setCertData(new_num);
+	// };
 	return (
 		<div className="flex flex-row justify-center items-center">
 			{/* <Cert

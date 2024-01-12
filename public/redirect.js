07cc0fb8256cbb8c6dc0b7f93784720332fd36a6
Aryan Github <34622095+Aryanamish@users.noreply.github.com>
2024-01-12 17:42:31 +0530

    added songstreamhub repo url to redirects

--- public/redirect.js
+++ public/redirect.js
@@ -1,13 +1,21 @@
 (() => {
-	const REDIRECT_PATHS = {
+	const REPO = {
 		'/repo/events-sathyabama/':
 			'https://github.com/Events-Sathyabama/Events-Sathyabama/',
+		'/repo/songstreamhub/': 'https://github.com/Surya-Kumar-03/SongStreamHub',
+	};
+	const VIDEO = {
 		'/video/events-sathyabama/':
 			'https://www.youtube.com/watch?v=iLgYvN-qO4c&ab_channel=Surya',
 		'/video/vorkinsta/':
 			'https://www.youtube.com/watch?v=C60mzqOQOc4&pp=ygUJdm9ya2luc3Rh',
+	};
+	const SITE = {
 		'/site/events-sathyabama/': 'https://events.aryanamish.in/',
 		'/site/vorkinsta/': 'https://www.vorkinsta.com/',
+	};
+
+	const CERT = {
 		'/cert/cloud-digital-leader/':
 			'https://www.credential.net/56ec8bbe-a535-4aae-b6fc-603fac685ab6',
 		'/cert/nptel-python/':
@@ -19,14 +27,25 @@
 		'/cert/codingo/':
 			'https://drive.google.com/file/d/12z7n6cZeMlPuK6ueGW5fyKEDhEgBqLBp/view?usp=sharing',
 	};
+	const REDIRECT_PATHS = {
+		video: VIDEO,
+		repo: REPO,
+		site: SITE,
+		cert: CERT,
+	};
 
 	let path = window.location.pathname;
 	if (!path.endsWith('/')) {
 		path += '/';
 	}
-	if (path in REDIRECT_PATHS) {
-		window.location.replace(REDIRECT_PATHS[path]);
-	} else if (path !== '/') {
+	for (let sub_path_name in REDIRECT_PATHS) {
+		const sub_paths = REDIRECT_PATHS[sub_path_name];
+		if (path in sub_paths) {
+			window.location.replace(sub_paths[path]);
+			return;
+		}
+	}
+	if (path !== '/') {
 		window.location.replace('/');
 	}
 })();

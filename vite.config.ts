d2d748425545c2808907f3a04374cd68cc2606bd
aryan.amish <aryanamish385@gmail.com>
2025-02-28 04:27:47 +0530

    caching images also

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		VitePWA({
-		registerType: "autoUpdate",
-	}),
-],
-	build: {
-		assetsInlineLimit: 0, // Set to 0 to disable inlining completely
-		outDir: './build',
-	},
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|avif)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "image-cache",
+              expiration: {
+                maxEntries: 50, // Limit cache size
+                maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0, // Set to 0 to disable inlining completely
+    outDir: "./build",
+  },
 });
